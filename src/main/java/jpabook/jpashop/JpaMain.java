e55478bedbc9a799ca866a0676420415be493a6c
yjkim <jcdad3000@gmail.com>
2022-11-08 10:08:36 +0900

    실전 예제 4 - 상속관계 매핑

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -21,8 +22,13 @@ public class JpaMain {
 
         try{
 
-            Order order = new Order();
-            order.addOrderItem(new OrderItem());
+            Book book  = new Book();
+            book.setName("JPA");
+            book.setAuthor("KIM");
+            book.setIsbn("1000");
+
+
+            em.persist(book);
 
             tx.commit();
         }catch (Exception e){
--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -3,7 +3,7 @@ package jpabook.jpashop.domain;
 import javax.persistence.*;
 
 @Entity
-public class Delivery {
+public class Delivery extends BaseEntity{
 
     @Id @GeneratedValue
     private Long id;
@@ -11,7 +11,7 @@ public class Delivery {
     private String city;
     private String street;
     private String zipcode;
-    private DeliveryStatus status;
+    //private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery")
     private Order order;
