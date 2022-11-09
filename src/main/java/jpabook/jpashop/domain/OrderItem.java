07bcbbb5992f06e65369686bf813fe788c6d740d
yjkim <jcdad3000@gmail.com>
2022-11-09 15:34:12 +0900

    실전 예제 5

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -9,11 +9,11 @@ public class OrderItem extends BaseEntity{
     @Column(name="ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="ORDER_ID")
     private jpabook.jpashop.domain.Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="ITEM_ID")
     private jpabook.jpashop.domain.Item item;
 
